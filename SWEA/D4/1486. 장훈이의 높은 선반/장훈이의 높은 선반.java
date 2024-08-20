65f3333f0f1f9a819ae59065d0bc7780344595dd
Yeseul Gong <45984578+0ys@users.noreply.github.com>
2024-08-20 14:06:11 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 188 ms, Memory: 20,404 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int N, B, S;
+	static int[] H;
+	static boolean[] isSelected;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+	
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			H = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				H[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(H);
+			isSelected = new boolean[N];
+			S=Integer.MAX_VALUE;
+			SubSet(0);
+			
+			sb.append("#"+tc+" "+(S-B)+"\n");
+			
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void SubSet(int cnt) {
+		if(cnt == N) {
+			int sum = 0;
+			for(int i=0; i<N; i++) {
+				if(isSelected[i]) sum += H[i];
+			}
+			if(sum >= B) S = Math.min(S, sum);
+			return;
+		}
+		
+		isSelected[cnt] = true;
+		SubSet(cnt+1);
+		
+		isSelected[cnt] = false;
+		SubSet(cnt+1);
+		
+	}
+}
