0aee6f0fc4f933c1f04f3fe79b881ba6962b6b26
Yeseul Gong <45984578+0ys@users.noreply.github.com>
2024-10-02 10:13:59 +0900

    [Gold V] Title: 용액, Time: 332 ms, Memory: 28812 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
+/*
+반례
+7
+-99 -2 -1 1 98 100 101
+ */
 public class Main {
 	static int N; //2 이상 100,000 이하
 	static long inputs[];
 	// 산성 용액 : 1 ~ 10^9
 	// 알칼리성 용액 : -10^9 ~ -1
 	
-	static ArrayDeque<Long> answer = new ArrayDeque<>();
+	static long answer[];
 	// 같은 양의 두 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만드는 두 용액의 특성값을 오름차순으로 출력하라.
 	// 1. 한 종류의 용액만으로 특성값이 0에 가장 가까운 혼합 용액을 만드는 경우도 존재함
 	// 2. 용액의 특성값이 정렬된 순서로 주어짐
@@ -18,6 +22,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		inputs = new long[N];
+		answer = new long[2];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
@@ -25,45 +30,35 @@ public class Main {
 		}
 		
 		long min = Long.MAX_VALUE;
+		
 		// 완전 탐색 방식 -> 당연히 시간 초과 O(N*N)
 		/*for(int i=0; i<N; i++) {
 			for(int j=i; j<N; j++) {
 				if(i==j) continue;
 				if(Math.abs(inputs[i]+inputs[j]) < min) {
 					min = Math.abs(inputs[i]+inputs[j]);
-					answer.clear();
-					answer.add(inputs[i]);
-					answer.add(inputs[j]);
+					answer[0] = inputs[i];
+					answer[1] = inputs[j];
 				}
 			}
 		}*/
 		
 		// 투 포인터
-		/*
-		반례
-		7
-		-99 -2 -1 1 98 100 101
-		 */
 		int i=0, j=N-1;
 		while(i<j) {
 			long a = inputs[i];
 			long b = inputs[j];
 			if(Math.abs(a+b) < min) {
-				min = Math.abs(inputs[i]+inputs[j]);
-				answer.clear();
-				answer.add(a);
-				answer.add(b);
-				if(min == 0) break;
-				if(Math.abs(a) < Math.abs(b)) j--;
-				else i++;
+				min = Math.abs(a+b);
+				answer[0] = a;
+				answer[1] = b;
+				if(min == 0) break; //조기종료
 			} else if(a+b < 0) {
 				i++;
 			} else {
 				j--;
 			}
 		}
-		
-		
 		
 		for(long a : answer) {
 			System.out.print(a+" ");
