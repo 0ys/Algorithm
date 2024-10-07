4fef570323db0886f4e3ee778403ff655854447b
Yeseul Gong <45984578+0ys@users.noreply.github.com>
2024-10-07 22:25:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 484 ms, Memory: 96156 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,55 +2,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-    static int N, maxLength;
-    static int[] B = new int[1000001];
-    static int[] A = new int[1000001];
-    static int[] D = new int[1000001];
-    static int[] ans = new int[1000001];
+    static int N, A[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        A = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++) {
+        for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
-        int index;
-        B[++maxLength] = A[1];
-        D[1] = 1;
-        for(int i=2; i<=N; i++) {
-            if (B[maxLength] < A[i]) { // 가장 마지막 수열보다 현재 수열이 클 때
-                B[++maxLength] = A[i];
-                D[i] = maxLength;
-            } else {
-                index = binarySearch(1, maxLength, A[i]);
-                B[index] = A[i];
-                D[i] = index;
+
+//        int[] LIS = new int[N];
+//        for(int i = 0; i < N; i++){
+//            LIS[i] = 1;
+//            for(int j = 0; j < i; j++){
+//                if(A[j] < A[i] && LIS[i] < LIS[j]+1){
+//                    LIS[i] = LIS[j]+1;
+//                }
+//            }
+//        }
+//
+//        int max = 0;
+//        for(int a : LIS){
+//            if(max < a) max = a;
+//        }
+
+        System.out.println(findLIS(A));
+    }
+
+    // LIS를 찾는 메서드
+    public static int findLIS(int[] nums) {
+        int[] lis = new int[nums.length]; // LIS를 저장할 배열
+        int length = 0;  // LIS 배열의 실제 길이
+
+        for (int num : nums) {
+            // lis 배열에서 num이 들어갈 위치를 이진 탐색으로 찾음
+            int pos = Arrays.binarySearch(lis, 0, length, num);
+
+            // 이진 탐색 결과가 음수이면 삽입해야 할 위치를 나타내므로 (-pos - 1)로 변환
+            if (pos < 0) {
+                pos = -(pos + 1);
             }
-        }
-        System.out.println(maxLength);
-        index = maxLength;
-        int x = B[maxLength]+1;
-        for(int i=N; i >= 1; i--) {
-            if(D[i] == index && A[i] < x){
-                ans[index] = A[i];
-                x = A[i];
-                index--;
+
+            // 해당 위치에 num을 삽입하거나 기존 값을 대체
+            lis[pos] = num;
+
+            // 새로 추가된 경우 LIS 배열의 길이를 증가시킴
+            if (pos == length) {
+                length++;
             }
         }
 
-//        for(int i=1; i<=maxLength; i++){
-//            System.out.print(ans[i]+" ");
-//        }
-    }
-    static int binarySearch(int l, int r, int now) {
-        int mid;
-        while(l < r){
-            mid = (l+r) / 2;
-            if(B[mid] < now) l = mid+1;
-            else r = mid;
-        }
-        return l;
+        // LIS 배열의 실제 길이가 최대 증가 부분 수열의 길이
+        return length;
     }
 }
